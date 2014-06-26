2d9895a072355a13771e00833b1ca213b79f43fa
Alex Tsokurov <me@ximik.net>
2014-06-27 01:03:11 +0300

    text demo fix

--- jquery.dummyslider.js
+++ jquery.dummyslider.js
@@ -1,6 +1,7 @@
 (function( $ ) {
   var DummySlider = function DummySlider(node, opts) {
     var _ = this;
+    _.opts = opts;
     _.viewport = $(node);
     _.controls = null;
     _.pager = null;
@@ -10,7 +11,7 @@
     if (opts.controls) _.buildControls();
     if (opts.pager) _.buildPager();
     _.slide = 0;
-    _.position();
+    _.position(true);
   };
   DummySlider.prototype.buildNodes = function () {
     var _ = this;
@@ -32,8 +33,8 @@
   DummySlider.prototype.buildPager = function () {
     var _ = this;
     var listener = function () {
-      _.currentSlide = $(this).data("slide");
-      _.positionSlides();
+      _.slide = $(this).data("slide");
+      _.position();
     };
     _.pager = $("<ul>").addClass("dummyslider-pager").appendTo(_.wrapper);
     var i, l = _.pos.length;
@@ -54,34 +55,62 @@
   };
   DummySlider.prototype.calc = function () {
     var _ = this;
+    var pos = _.pos = [];
     var children = _.viewport.children();
     var w = 0, width = _.viewport.width();
     var f, t, wt;
-    var i, j, l = children.length;
-    _.pos = [];
-    for (i = 0; i < l; i++) {
-      _.pos.push(w);
-      wt = $(children[i]).outerWidth() + parseInt($(children[i]).css("marginRight"), 10);
-      for (j = i + 1; j < l; j++) {
-        t = $(children[j]).outerWidth() + parseInt($(children[j]).css("marginLeft"), 10);
-        if (wt + t> width) {
-          i = j - 1;
-          break;
+    var i, j, br = 0, l = children.length;
+    switch (_.opts.align) {
+      case "left":
+        for (i = 0; i < l; i++) {
+          pos.push(w);
+          wt = $(children[i]).outerWidth() + parseInt($(children[i]).css("marginRight"), 10);
+          for (j = i + 1; j < l; j++) {
+            t = $(children[j]).outerWidth() + parseInt($(children[j]).css("marginLeft"), 10);
+            if (wt + t > width) {
+              i = j - 1;
+              br++;
+              break;
+            }
+            wt += t;
+          }
+          w += wt;
         }
-        wt += t;
-      }
-      w += wt;
+        if (br + 1 === pos.length) {
+          pos[pos.length - 1] += t - width;
+        } else {
+          pos.pop();
+        }
+        break;
+      case "center":
+      case "middle":
+        for (i = 0; i < l; i++) {
+          wt = $(children[i]).outerWidth(true);
+          w += (width - wt) / 2;
+          pos.push(w);
+        }
+        break;
     }
+    console.log(pos);
   };
-  DummySlider.prototype.position = function () {
+  DummySlider.prototype.position = function (quiet) {
+    quiet = quiet === undefined ? false : true;
     var viewport = this.viewport;
     var pos = -this.pos[this.slide];
-    viewport.removeClass("dummyslider-transition");
+    if (quiet) {
+      viewport.removeClass("dummyslider-transition");
+    }
     viewport.children().each(function () {
       $(this).css("left", pos + "px");
       pos += $(this).outerWidth() + parseInt($(this).css("marginRight"), 10);
     });
-    viewport.addClass("dummyslider-transition");
+    if (quiet) {
+      viewport.addClass("dummyslider-transition");
+    }
+    if (this.pager !== null) {
+      this.pager.find(".current").removeClass("current");
+      this.pager.find("[data-slide=" + this.slide + "]").addClass("current");
+    }
   };
   DummySlider.prototype.prev = function () {
     if (this.slide === 0) return;
@@ -108,6 +137,8 @@
   $.fn.dummyslider.defaults = {
     controls: true,
     pager: true,
+    align: "left",
+    valign: "middle",
     loop: false
   };
 }(jQuery));
