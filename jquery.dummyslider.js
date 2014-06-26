4e7cb0c8177e1e783c343335de220de2792dc867
Alex Tsokurov <me@ximik.net>
2014-06-26 21:36:23 +0300

    mixed-widths

--- jquery.dummyslider.js
+++ jquery.dummyslider.js
@@ -1,22 +1,21 @@
 (function( $ ) {
   var DummySlider = function DummySlider(node, opts) {
-    this.viewport = node;
-    this.controls = null;
-    this.pager = null;
-    this.currentSlide = 0;
-    this.buildNodes();
-    if (opts.controls) this.buildControls();
-    if (opts.pager) this.buildPager();
-    this.position();
+    var _ = this;
+    _.viewport = $(node);
+    _.controls = null;
+    _.pager = null;
+    _.buildNodes();
+    _.size();
+    _.calc();
+    if (opts.controls) _.buildControls();
+    if (opts.pager) _.buildPager();
+    _.slide = 0;
+    _.position();
   };
   DummySlider.prototype.buildNodes = function () {
     var _ = this;
     _.viewport.addClass("dummyslider-viewport");
-    _.wrapper = _.viewport.wrap("<div class='dummyslider-wrapper'></div>").parent();
-    _.slides =  _.viewport.children();
-    _.slides.each(function (i) {
-      $(this).addClass("dummyslider-slide").attr("data-index", i);
-    });
+    _.wrapper = _.viewport.wrap("<div></div>").parent().addClass("dummyslider-wrapper");
   };
   DummySlider.prototype.buildControls = function () {
     var _ = this;
@@ -33,71 +32,82 @@
   DummySlider.prototype.buildPager = function () {
     var _ = this;
     var listener = function () {
-      _.currentSlide = $(this).data("index");
+      _.currentSlide = $(this).data("slide");
       _.positionSlides();
     };
     _.pager = $("<ul>").addClass("dummyslider-pager").appendTo(_.wrapper);
-    var i, l = _.slides.length;
+    var i, l = _.pos.length;
     for (i = 0; i < l; i++) {
-      $("<li>").attr("data-index", i).appendTo(_.pager).click(listener);
+      $("<li>").attr("data-slide", i).appendTo(_.pager).click(listener);
     }
   };
-  DummySlider.prototype.positionSlides = function () {
-    var left = 0;
-    var slides = this.slides;
-    var i, l = this.currentSlide;
-    for (i = 0; i < l; i++) {
-      left -= $(slides[i]).outerWidth(true);
-    }
-    l = slides.length;
-    for (i = 0; i < l; i++) {
-      $(slides[i]).css("left", left + "px");
-      left += $(slides[i]).outerWidth(true);
-    }
-    if (this.pager !== null) {
-      this.pager.find("li.current").removeClass("current");
-      this.pager.find("li[data-index=" + this.currentSlide + "]").addClass("current");
-    }
-  };
-  DummySlider.prototype.position = function () {
-    this.viewport.removeClass("dummyslider-transition");
+  DummySlider.prototype.size = function () {
+    var _ = this;
     var height = 0;
-    this.slides.each(function () {
+    _.viewport.children().each(function () {
       var h = this.offsetHeight;
       if (h > height) {
         height = h;
       }
     });
-    this.viewport.height(height);
-    this.positionSlides();
-    this.viewport.addClass("dummyslider-transition");
+    _.viewport.height(height);
+  };
+  DummySlider.prototype.calc = function () {
+    var _ = this;
+    var children = _.viewport.children();
+    var w = 0, width = _.viewport.width();
+    var f, t, wt;
+    var i, j, l = children.length;
+    _.pos = [];
+    for (i = 0; i < l; i++) {
+      _.pos.push(w);
+      wt = $(children[i]).outerWidth() + parseInt($(children[i]).css("marginRight"), 10);
+      for (j = i + 1; j < l; j++) {
+        t = $(children[j]).outerWidth() + parseInt($(children[j]).css("marginLeft"), 10);
+        if (wt + t> width) {
+          i = j - 1;
+          break;
+        }
+        wt += t;
+      }
+      w += wt;
+    }
+  };
+  DummySlider.prototype.position = function () {
+    var viewport = this.viewport;
+    var pos = -this.pos[this.slide];
+    viewport.removeClass("dummyslider-transition");
+    viewport.children().each(function () {
+      $(this).css("left", pos + "px");
+      pos += $(this).outerWidth() + parseInt($(this).css("marginRight"), 10);
+    });
+    viewport.addClass("dummyslider-transition");
   };
   DummySlider.prototype.prev = function () {
-    if (this.currentSlide === 0) return;
-    this.currentSlide--;
-    this.positionSlides();
+    if (this.slide === 0) return;
+    this.slide--;
+    this.position();
   };
   DummySlider.prototype.next = function () {
-    if (this.currentSlide + 1 === this.slides.length ) return;
-    this.currentSlide++;
-    this.positionSlides();
+    if (this.slide + 1 === this.pos.length ) return;
+    this.slide++;
+    this.position();
   };
-  
+
   $.fn.dummyslider = function(options) {
     var opts = $.extend({}, $.fn.dummyslider.defaults, options);
 
     return this.map(function() {
-      var node = $(this);
-      var slider = $(this).data("dummyslider");
+      var slider = this.dummyslider;
       if (slider === undefined) {
-        slider = new DummySlider(node, opts);
-        $(this).data(slider);
+        slider = this.dummyslider = new DummySlider(this, opts);
       }
       return slider;
     });
   };
   $.fn.dummyslider.defaults = {
     controls: true,
-    pager: true
+    pager: true,
+    loop: false
   };
-}(jQuery)); 
+}(jQuery));
